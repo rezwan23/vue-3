984e1ae8b3ce8f428682cde02d216c1cb1563f45
Md Shariful Islam <mislam151243@bscse.uiu.ac.bd>
2023-10-16 18:31:59 +0600

    forum post details page modified.

--- src/pages/apps/forum/view/[id].vue
+++ src/pages/apps/forum/view/[id].vue
@@ -40,56 +40,35 @@ const fetcPost = () => {
     })
 }
 
-
-
-
 watchEffect(fetcPost);
 
-
-const solidCardData = [
-    {
-        cardBg: '#007BB6',
-        title: 'Linkedin Card',
-        icon: 'tabler-brand-linkedin',
-        text: 'With the Internet spreading like wildfire and reaching every part of our daily life, more and more traffic is directed.',
-        avatarImg: avatar8,
-        avatarName: 'Anne Burke1',
-        likes: '1.2k',
-        share: '80',
-    },
-]
 </script>
 
 <template>
     <VRow v-if="isPost">
         <VCol :key="'tabler-ad-2'" cols="12" md="12">
             <VCard :color="'#007BB6'">
-                <VCardItem>
-                    <template #prepend>
-                        <VIcon size="1.9rem" color="white" :icon="'tabler-ad-2'" />
-                    </template>
-                    <VCardTitle class="text-white">
-                        {{ post.title }}
-                    </VCardTitle>
-                </VCardItem>
 
-                <VCardText>
-                    <p class="clamp-text text-white mb-0">
-                        {{ post.description }}
-                    </p>
-                </VCardText>
-
-                <VCardText class="d-flex justify-space-between align-center flex-wrap">
-                    <div class="text-no-wrap">
+                <VCardText class="d-flex justify-space-between align-center flex-wrap mt-2">
+                    <div class="text-no-wrap d-flex justify-start">
                         <VAvatar size="34" :image="post.user.image" />
-                        <span class="text-white ms-2">{{ post.user.name }}</span>
+                        <h3 class="text-white ms-2">{{ post.user.name }}</h3>
                     </div>
-
-                    <div class="d-flex align-center">
-                        <IconBtn icon="tabler-message-circle" color="white" class="me-1" />
-                        <span class="text-subtitle-2 text-white me-4">{{ post.total_comments }}</span>
-                    </div>
+                  <div class="d-flex align-center">
+                    <IconBtn icon="tabler-message-circle" color="white" class="me-1" />
+                    <span class="text-subtitle-2 text-white me-4">{{ post.total_comments }}</span>
+                  </div>
                 </VCardText>
+                <div class="ml-4">
+                  <VCardTitle class="text-white my-n1">
+                    {{ post.title }}
+                  </VCardTitle>
+                  <VCardText>
+                    <p class="clamp-text text-white mb-6">
+                      {{ post.description }}
+                    </p>
+                  </VCardText>
+                </div>
             </VCard>
         </VCol>
         <VCol>
@@ -109,21 +88,20 @@ const solidCardData = [
                     <VTimeline side="end" align="start" truncate-line="both" density="compact"
                         class="v-timeline-density-compact">
                         <VTimelineItem v-for="comment in post.comments" dot-color="primary" size="x-small">
-                            <!-- 👉 Header -->
+                          <div class="d-flex align-center">
+                            <VAvatar :image="comment.user.image" class="me-3" />
+                            <div>
+                              <p class="text-high-emphasis mb-n1">
+                                {{ comment.user.name }}
+                              </p>
+                            </div>
+                          </div>
                             <div class="d-flex justify-space-between align-center flex-wrap mb-1">
-                                <span class="app-timeline-title">
+                                <span class="app-timeline-title ml-5 pl-5">
                                     {{comment.description}}
                                 </span>
                             </div>
 
-                            <div class="d-flex align-center">
-                                <VAvatar :image="comment.user.image" class="me-3" />
-                                <div>
-                                    <p class="text-high-emphasis mb-n1">
-                                        {{ comment.user.name }}
-                                    </p>
-                                </div>
-                            </div>
                         </VTimelineItem>
                     </VTimeline>
                 </VCardText>
